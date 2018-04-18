6d7c32eda2e4f0afc636c30f0adb2a5314557981
jmirav <jmira420@gmail.com>
2018-04-17 21:26:55 -0500

    Set API for creating products

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -2,4 +2,18 @@ class Api::V1::ProductsController < ApplicationController
   def index
     render json: Product.all
   end
+
+  def create
+    product = Product.new(product_params)
+    if product.save
+      render json: product
+    else
+      render json: {errors: product.errors}, status: 422
+    end
+  end
+
+  private
+    def product_params
+      params.permit(:name, :price)
+    end
 end
